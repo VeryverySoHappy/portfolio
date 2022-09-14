98cd78a19981b2988cec14e6772190ecfeff1fdd
VeryverySoHappy <VeryverySoHappy>
2022-09-14 09:42:15 +0900

    main, contact, about

--- js/main.js
+++ js/main.js
@@ -1,56 +1,42 @@
-// $(".section").each(function () {
+$(".section").each(function () {
+  $(this).on("mousewheel DOMMouseScroll", function (e) {
+    e.preventDefault();
+    var delta = 0;
+    if (!event) event = window.event;
+    if (event.wheelDelta) {
+      delta = event.wheelDelta / 120;
+      if (window.opera) delta = -delta;
+    } else if (event.detail) delta = -event.detail / 3;
+    var moveTop = null;
+    if (delta < 0) {
+      if ($(this).next() != undefined) {
+        moveTop = $(this).next().offset().top;
+      }
+    } else {
+      if ($(this).prev() != undefined) {
+        moveTop = $(this).prev().offset().top;
+      }
+    }
+    $("html,body").stop().animate({
+      scrollTop: moveTop + 'px'
+    }, {
+      duration: 800,
+      complete: function () {
+      }
+    });
+  });
+});
 
-//   // 개별적으로 Wheel 이벤트 적용
-
-//   $(this).on("mousewheel DOMMouseScroll", function (e) {
-
-//     e.preventDefault();
-
-//     var delta = 0;
-
-//     if (!event) event = window.event;
-
-//     if (event.wheelDelta) {
-
-//       delta = event.wheelDelta / 120;
-
-//       if (window.opera) delta = -delta;
-
-//     } else if (event.detail) delta = -event.detail / 3;
-
-//     var moveTop = null;
-
-//     if (delta < 0) {
-
-//       if ($(this).next() != undefined) {
-
-//         moveTop = $(this).next().offset().top;
-
-//       }
-
-//     } else {
-
-//       if ($(this).prev() != undefined) {
-
-//         moveTop = $(this).prev().offset().top;
-
-//       }
-
-//     }
-
-//     $("html,body").stop().animate({
-
-//       scrollTop: moveTop + 'px'
-
-//     }, {
-
-//       duration: 800,
-//       complete: function () {
-
-//       }
-
-//     });
-
-//   });
-
-// });
+// about start
+$('.navi li').mouseenter(function(){
+  var changeImage = $(this).attr('data-image')
+  $('.photo').css({
+    'background-image': 'url('+ changeImage +')'
+  })
+})
+$('.navi li').mouseleave(function(){
+  $('.photo').css({
+    'background-image': ''
+  })
+})
+// about end
--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,31 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper2 = new Swiper('.mySwiper', {
+  speed: 500,
   slidesPerView: 1,
   spaceBetween: 30,
   mousewheel: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
+  watchOverflow : true,
+  on: {
+      slideChange: function() {
+          setTimeout(function () {
+            swiper2.params.touchReleaseOnEdges = false;  
+            swiper2.params.mousewheel.releaseOnEdges = false;
+          });
+      },
+      reachEnd: function() {
+          setTimeout(function () {
+              swiper2.params.touchReleaseOnEdges = true;
+              swiper2.params.mousewheel.releaseOnEdges = true;
+          }, 500);
+      },
+      reachBeginning: function() {
+          setTimeout(function () {
+              swiper2.params.touchReleaseOnEdges = true;
+              swiper2.params.mousewheel.releaseOnEdges = true;
+          }, 500);
+      }
+  }
 });
